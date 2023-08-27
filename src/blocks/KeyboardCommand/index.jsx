24a6d292eaea8306547b975b8167a8adee5bd13c
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-08-27 19:17:36 +0530

    feat: created view menu

--- src/blocks/KeyboardCommand/index.jsx
+++ src/blocks/KeyboardCommand/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Kbd = ({ children }) => {
-  return <kbd className="font-semibold text-sm text-gray-light">{children}</kbd>;
+  return (
+    <kbd className="font-semibold text-sm text-gray-light">{children}</kbd>
+  );
 };
